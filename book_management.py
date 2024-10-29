dbb9585af04215cd19ddbf7e78d5c7d72f3bfb70
Natalia Novikova <novikova.nm@phystech.edu>
2024-10-29 19:04:52 +0300

    созданы методы для работы с библиотекой

---
+++ book_management.py
@@ -0,0 +1,72 @@
+from typing import List, Optional
+
+class Book:
+    def __init__(self, name: str, author: str, year: int, available: bool):
+        self.name = name
+        self.author = author
+        self.year = year
+        self.available = available
+
+class BookManagementSystem:
+    def __init__(self):
+        self.books: List[Book] = []
+
+    def add_book(self, book: Book) -> bool:
+        if any (b.name == book.name and
+                b.author == book.author and
+                b.year == book.year for b in self.books ):
+            return False # book already exists
+        self.books.append(book)
+        return True
+
+    def find_book(self, name: str, author: str, year: int) -> Optional[Book]:
+        for book in self.books:
+            if (book.name == name
+                    and book.author == author
+                    and book.year == year
+                    and book.available):  # Ищем доступную книгу по названию
+                return book
+        return None
+
+    def is_book_borrow(self ,name: str, author: str, year: int) -> bool:
+        for book in self.books:
+            if (book.name == name
+                    and book.author == author
+                    and book.year == year
+                    and not book.available):
+                return True
+        return False
+
+    def return_book(self, name: str, author: str, year: int) -> bool:
+        for book in self.books:
+            if (book.name == name
+                    and book.author == author
+                    and book.year == year
+                    and not book.available):
+                book.available = True
+                return True
+        return False
+
+if __name__ == '__main__':
+    library = BookManagementSystem()
+    book1 = Book("The Lord of the Rings", "J. R. R. Tolkien", 1954, True)
+    book2 = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979, True)
+
+    library.add_book(book1)
+    library.add_book(book2)
+
+    found_book = library.find_book("The Lord of the Rings", "J. R. R. Tolkien", 1954)
+    if found_book:
+        book1.available = False
+        print(f"Found book: {found_book.name} by {found_book.author} ({found_book.year})")
+
+    print(f"Is 'The Hitchhiker's Guide to the Galaxy' borrowed?"
+          f" {library.is_book_borrow('The Hitchhiker\'s Guide to the Galaxy',
+          'Douglas Adams', 1979)}"
+          f"\nIs 'The Lord of the Rings' borrowed? {library.is_book_borrow('The Lord of the Rings', 
+                                                                           "J. R. R. Tolkien", 1954)}")
+
+
+
+
+
